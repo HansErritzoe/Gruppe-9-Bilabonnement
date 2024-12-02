18d5f9161d472d8c3780dec540393813e91d19cd
HansErritzoe <h.erritzoe@hotmail.com>
2024-12-02 13:59:04 +0100

    Initial commit

---
+++ src/main/java/org/example/gruppe9bilabonnement/Gruppe9BilabonnementApplication.java
@@ -0,0 +1,13 @@
+package org.example.gruppe9bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe9BilabonnementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruppe9BilabonnementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gruppe9bilabonnement/Gruppe9BilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gruppe9bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe9BilabonnementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
