a5e2390bc8dd94074be3618646dc390b64a80784
jonascjak <jonascjak@gmail.com>
2024-12-07 10:47:38 +0100

    Made add rental contract function, without added css

--- src/main/java/org/example/gruppe9bilabonnement/Controller/HomeController.java
+++ src/main/java/org/example/gruppe9bilabonnement/Controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.example.gruppe9bilabonnement.Controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.example.gruppe9bilabonnement.Model.Rental_contract;
+import org.example.gruppe9bilabonnement.Service.RentalContractService;
 import org.example.gruppe9bilabonnement.Service.UserService;
 import org.example.gruppe9bilabonnement.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -16,7 +19,8 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-
+    @Autowired
+    RentalContractService rentalContractService;
     /**
      * method for returning the login page for when user isn't logged in
      * @return string with html page location/name
@@ -37,6 +41,41 @@ public class HomeController {
     public String dashboard(HttpSession session, Model model){
         if(userIsLoggedIn(session)){
             return "dashboard/dashboard";
+        } else {
+            model.addAttribute("loginErrorMessage", "Du er ikke logget ind - log ind for at kunne tilgå denne side");
+            return "login/loginPage";
+        }
+    }
+
+    /**
+     * Method for displaying the add_rentalcontract html page where user can add rental contracts to the database
+     * @param session - HTTPSession object used for checking that user is logged in
+     * @param model - Model object used for displaying error message if user attempts to access without loggin in
+     * @return string - returns the string of the html file address with the add_rentalcontract page or login if not logged in
+     * @Author Jonas Jakobsen
+     */
+    @GetMapping("/rental_contract/add_rentalcontract")
+    public String addRentalcontract(HttpSession session, Model model){
+        if(userIsLoggedIn(session)){
+            return "/rental_contract/add_rentalcontract";
+        } else {
+            model.addAttribute("loginErrorMessage", "Du er ikke logget ind - log ind for at kunne tilgå denne side");
+            return "login/loginPage";
+        }
+    }
+
+    /**
+     * Method for returning the rental_contract html file address
+     * @return string with rental_contract address if user is logged in, else it returns the user to the login page with error message
+     * @Author Jonas Jakobsen
+     */
+    @GetMapping("/rental_contract")
+    public String rental_contract(HttpSession session, Model model){
+        if(userIsLoggedIn(session)){
+
+            List<Rental_contract> rentalContracts = rentalContractService.getAllRentalcontracts();
+            model.addAttribute("rental_contract",rentalContracts);
+            return "rental_contract/rental_contract";
         } else {
             model.addAttribute("loginErrorMessage", "Du er ikke logget ind - log ind for at kunne tilgå denne side");
             return "login/loginPage";
---
+++ src/main/java/org/example/gruppe9bilabonnement/Repository/RentalcontractRepository.java
@@ -0,0 +1,49 @@
+package org.example.gruppe9bilabonnement.Repository;
+
+import org.example.gruppe9bilabonnement.Model.Rental_contract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class RentalcontractRepository {
+
+    @Autowired
+    JdbcTemplate template;
+
+
+    /**
+     * Method for returning a list of all rentalContracts from the DB
+     * @return List<Rentalcontract> - list of all rentalcars
+     * @Author Jonas Jakobsen
+     */
+    public List<Rental_contract> getAllRentalcontracts(){
+        String sql = "SELECT * FROM rental_contract";
+        RowMapper<Rental_contract> rowMapper = new BeanPropertyRowMapper<>(Rental_contract.class);
+        return template.query(sql,rowMapper);
+    }
+
+    /**
+     * Method for adding the car to the database, returns false if failure, true if not
+     * @param rental_contract - rental to be added to the database
+     * @return boolean - false if failed to add car to db, true if added successfully
+     * @Author Hans Erritzøe
+     */
+    public boolean addRental_contract(Rental_contract rental_contract){
+        String sql = """
+                    INSERT INTO rental_contract (customer_id, car_vehicle_id, start_date, end_date, status, irk_code, leasing_code, km_pr_month, price_per_month, payment_status, pickup_location, return_location, id_damage_report)
+                     VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+                    """;
+        try {
+            int rowsAffected = template.update(sql, rental_contract.getCustomer_id(),rental_contract.getCar_vehicle_id(),rental_contract.getStart_date(), rental_contract.getEnd_date(), rental_contract.getStatus(), rental_contract.getIrk_code(),rental_contract.getLeasing_code(),rental_contract.getKm_pr_month(), rental_contract.getPrice_per_month(),rental_contract.getPayment_status(), rental_contract.getPickup_location(), rental_contract.getReturn_location(), rental_contract.getId_damage_report());
+            return rowsAffected > 0;
+        } catch (DataAccessException errorMessage){
+            return false;
+        }
+    }
+}
